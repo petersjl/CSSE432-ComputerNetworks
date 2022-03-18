6d461f2cfee9e93bbc4807d13ecba0d3549ace13
Peters-Joseph <petersjl@rose-hulman.edu>
2022-03-17 20:56:27 -0400

    lab0 init

---
+++ Labs/Lab0/paint.c
@@ -0,0 +1,17 @@
+/* File: paint.c 
+   Author: 
+   
+*/
+#define CAN_COVERAGE 200
+
+#include <stdio.h>
+#include <math.h>
+
+/*Optional functions, uncomment the next two lines
+ * if you want to create these functions after main: */
+//float readDimension(const char* name);
+//float calcArea(float width, float height, float depth);
+
+int main(int argc, char *argv[]){
+    //your code here
+}
