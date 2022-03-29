37b3e123f0b6e16637f49f043ef7222505ddcf89
Peters-Joseph <petersjl@rose-hulman.edu>
2022-03-28 21:32:59 -0400

    server now has more accurate messages

--- Labs/Lab1/node/server.js
+++ Labs/Lab1/node/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const readline = require('readline');
 var bodyParser = require("body-parser");
+const os = require('os');
 
 const server = express()
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+const hostname = os.hostname();
 
 let port;
 if (process.argv[2] == null) {
@@ -21,13 +24,19 @@ if (process.argv[2] == null) {
 	port = parseInt(process.argv[2], 10);
 }
 
+let dict = {};
+
 server.use('/', bodyParser.json());
 
 server.post('/', (req, res) => {
-	console.log(`Got message: ${req.body.message}`);
+	console.log(`${req.hostname} says: ${req.body.message}`);
 	let m = req.body.message.toUpperCase();
-	console.log(`New message: ${m}`);
-	res.send(JSON.stringify({message: m}));
+	if(dict[req.hostname] == null){
+		dict[req.hostname] = 0;
+	}
+	dict[req.hostname] += 1;
+	console.log(`Replying to ${req.hostname} message ${dict[req.hostname]} with message in upper case`);
+	res.send(JSON.stringify({message: dict[req.hostname] + " " + m}));
 });
-console.log(`Starting server on ${server.name} on port ${port}`);
+console.log(`Starting server on ${hostname} on port ${port}`);
 server.listen(port);
