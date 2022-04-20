4ef2564985d364f50f1f39093bd56fa1f0cd99d2
petersjl <petersjl@rose-hulman.edu>
2022-04-20 14:21:20 -0400

    lab2 node list function

--- Labs/Lab2/node/client/client.js
+++ Labs/Lab2/node/client/client.js
@@ -1,49 +1,115 @@
 const http = require('http');
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
+const DEFAULT_DIRECTORY = __dirname + "/store";
+
 if (process.argv[3] == null){
 	console.log("Usage: node clinet.js host port.");
 	process.exit();
 }
 
-const options = {
-  hostname: process.argv[2],
-  port: process.argv[3],
-  path: '/',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+const listOptions = {
+	hostname: process.argv[2],
+	port: process.argv[3],
+	path: '/list',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	}
 };
 
-console.log(`Talking to host: ${options.hostname} on port ${options.port}\nType ';;;' to exit\n`);
+const takeOptions = {
+	hostname: process.argv[2],
+	port: process.argv[3],
+	path: '/take',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+};
+
+const wantOptions = {
+	hostname: process.argv[2],
+	port: process.argv[3],
+	path: '/want',
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json'
+	}
+};
+
+console.log(`Talking to host: ${listOptions.hostname} on port ${listOptions.port}\nType ';;;' to exit\n`);
 
 function askQuestion(){
-	rl.question("You> ", function (p) {
+	rl.question("> ", function (p) {
 	if (p == ";;;"){
 		console.log('Bye!');
 		process.exit();
 	}else{
-		const req = http.request(options, res => {  
-			res.on('data', d => {
-			  	console.log(`Srv> ${JSON.parse(d).message}`);
+		let parts = p.split(" ");
+		let command = parts[0].toLowerCase();
+		switch (command){
+			case "ls": 
+			case "list": {
+				list(parts[1] || "/");
+			}break;
+			case "iwant" : {
+				if(!parts[1]) {
+					console.log("\tUsage: iWant <file path on server> (destination on client)\n");
+					askQuestion();
+				}
+				want(parts[1], parts[2] || DEFAULT_DIRECTORY);
+			}break;
+			case "utake" : {
+				if(!parts[1]) {
+					console.log("\tUsage: uTake <file path on client> (destination on server)\n");
+					askQuestion();
+				}
+				take(parts[1], parts[2] || DEFAULT_DIRECTORY);
+			}break;
+			default : {
+				console.log("Available commands:\nls (path)\niWant <file path on server> (destination on client)\nuTake <file path on client> (destination on server)\n");
 				askQuestion();
-			})
-		})
-		  
-		req.on('error', error => {
-			console.error(error)
-		})
-
-		req.write(JSON.stringify({message: p}));
-		req.end();
+			}
+		}
 	}
 	})
+}
+
+function list(path){
+	console.log(path);
+	const req = http.request(listOptions, res => {  
+		res.on('data', d => {
+			let data = JSON.parse(d);
+			if(data.error){
+				console.log("Error: path not found");
+			}else{
+				console.log(`${path}> ${data.message}`);
+			}
+			askQuestion();
+		})
+	})
+		
+	req.on('error', error => {
+		console.error(error)
+	})
+
+	req.write(JSON.stringify({path: path}));
+	req.end();
+}
+
+function take(from, to){
+
+}
+
+function want(from, to){
+
 }
 
 askQuestion();
--- Labs/Lab2/node/server/server.js
+++ Labs/Lab2/node/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const readline = require('readline');
 var bodyParser = require("body-parser");
 const os = require('os');
+const fs = require('fs');
 
 const server = express()
 const rl = readline.createInterface({
@@ -10,6 +11,7 @@ const rl = readline.createInterface({
 });
 
 const hostname = os.hostname();
+const DEFAULT_DIRECTORY = __dirname + "/store";
 
 let port;
 if (process.argv[2] == null) {
@@ -27,6 +29,9 @@ if (process.argv[2] == null) {
 let dict = {};
 
 server.use('/', bodyParser.json());
+server.use('/list', bodyParser.json());
+server.use('/want', bodyParser.json());
+server.use('/take', bodyParser.json());
 
 server.post('/', (req, res) => {
 	console.log(`${req.hostname} says: ${req.body.message}`);
@@ -38,5 +43,21 @@ server.post('/', (req, res) => {
 	console.log(`Replying to ${req.hostname} message ${dict[req.hostname]} with message in upper case`);
 	res.send(JSON.stringify({message: dict[req.hostname] + " " + m}));
 });
+
+server.post("/list", (req, res) => {
+	let path = DEFAULT_DIRECTORY + req.body.path;
+	console.log(`Finding files at path: ${path}\nIn path: ${__dirname}`);
+	fs.readdir(path, (err, files) => {
+		if(err){
+			console.log("Incorrect path");
+			res.send(JSON.stringify({error: true}));
+		}else{console.log(`Found files: ${files}`);
+		let fileNames = "";
+		files.forEach(file => {fileNames += file + "\t"});
+		res.send(JSON.stringify({error: false, message: fileNames}));
+		}		
+	})
+})
+
 console.log(`Starting server on ${hostname} on port ${port}`);
 server.listen(port);
